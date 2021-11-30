c93822f669e4d0ed07a9c6d7296d6b99c113ca1d
rruefenacht <rruefenacht@users.noreply.github.com>
2021-11-30 17:28:25 +0100

    prototype for generating Measurement class from RPG

--- mwr_raw2l1/measurement.py
+++ mwr_raw2l1/measurement.py
@@ -1,5 +1,7 @@
 import xarray as xr
 
+from mwr_raw2l1.measurement_helpers import make_dataset
+
 
 class Measurement(object):
     def __init__(self):
@@ -12,24 +14,35 @@ class Measurement(object):
         Args:
             readin_data: dictionary with list of instances for each RPG read-in class (keys correspond to filetype)
         """
-        # dimensions and variable names for usage with make_xr_dict
+        # dimensions and variable names for usage with make_dataset
         dims = {'brt': ['time', 'frequency'],
-                'blb': ['time', 'frequency', 'ele'],
+                'blb': ['time', 'frequency', 'scan_ele'],
                 'irt': ['time', 'wavelength'],
-                'met': 'time',
-                'hkd': 'time'}
+                'met': ['time'],
+                'hkd': ['time']}
         vars = {'brt': ['Tb', 'rainflag', 'ele', 'azi'],
-                'blb': ['Tb_scan', 'T_per_scan', 'rainflag', 'scan_quadrant', 'ele'],
+                'blb': ['Tb_scan', 'T_per_scan', 'rainflag', 'scan_quadrant'],
                 'irt': ['IRT', 'rainflag', 'ele', 'azi'],
                 'met': ['p', 'T', 'RH'],
-                'hkd': ['lat', 'lon']}
-        # TODO generate complete list of variables for MET and HKD (maybe with vars_optional)
+                'hkd': ['alarm']}
+        vars_opt = {'brt': [],
+                    'blb': [],
+                    'irt': [],
+                    'met': ['windspeed', 'winddir', 'rainrate'],
+                    'hkd': ['lat', 'lon', 'T_amb_1', 'T_amb_2', 'T_receiver_kband', 'T_receiver_vband',
+                            'Tstab_kband', 'Tstab_vband', 'flashmemory_remaining', 'statusflag']}
 
-        # TODO: work with d=make_xr_dict and xr.Dataset.from_dict(d) for each instance in read-in data
-        # TODO: combine with xr.concat or similar
-        # TODO: use HKD for evaluating scan time
+        out = cls
+        all_data = {}
+        for ext, data_series in readin_data.items():
+            data_act = []
+            for dat in data_series:  # make an xarray dataset from the data dict in each class instance
+                data_act.append(make_dataset(dat.data, dims[ext], vars[ext], vars_opt[ext]))
+            all_data[ext] = xr.concat(data_act, dim='time')  # merge all datasets of the same type
 
-
+        out.data = all_data  # TODO: merge BRT and BLB (use HKD for evaluating scan time) & interpolate irt, met, hkd
+        # TODO: ask Volker where to do transformation from BLB scan to timeseries. probably here as use HKD info. But it is still a very BLB-specific method
+        return out
 
     # TODO: Example code, remove when done
     @classmethod
@@ -45,4 +58,12 @@ class Measurement(object):
     # TODO: use 3 different class methods to read in data from the different instruments to a common measurement class
     # don't need a BaseMeasurement class in this case as only 1. Measurement class can then be handed over to writer
 
-    # TODO: for combining RPG files to time series use xarray dataset. For writing, minght maybe still use netCDF4
+    # TODO: for combining to time series use xarray dataset. For writing, might maybe still use netCDF4
+
+
+if __name__ == '__main__':
+    from mwr_raw2l1.reader_rpg import read_all
+
+    all_data = read_all('data/rpg/', 'C00-V859')
+    meas = Measurement.from_rpg(all_data)
+    pass
--- mwr_raw2l1/measurement_helpers.py
+++ mwr_raw2l1/measurement_helpers.py
@@ -1,23 +1,42 @@
 import numpy as np
+import xarray as xr
 
 from mwr_raw2l1.errors import DimensionError
 
 
-def make_xr_dict(data, dims, vars):
-    """generate a dictionary containing the dimensions and variables to initialise xarray dataset
+def make_dataset(data, dims, vars, vars_opt=None):
+    """generate a xarray Dataset from 'data' using the dimensions and variables specified
 
     Args:
         data: dictionary containing the data
         dims: list of keys that are a dimension (must correspond to the order of dimensions in data)
         vars: list of keys that are data variables (dimensions don't need to be specified again)
+        vars_opt: list of keys that are optional data variables (added as 1-dim series of NaN if missing in 'data')
+    Returns:
+        xarray.Dataset
     """
 
-    out = {}
-    for dim in dims:
-        out[dim] = dict(dims=dim, data=data[dim])
+    # init
+    spec = {}
+    if vars_opt is None:
+        vars_opt = []
 
-    for var in vars:
+    # add dimensions to spec
+    for dim in dims:
+        spec[dim] = dict(dims=dim, data=data[dim])
+
+    # add optional variables as NaN-series to data
+    for varo in vars_opt:
+        if varo not in data:
+            data[varo] = np.full_like(data[dims[0]], np.nan)
+            # TODO: add logger here to give info that varo is not in data
+
+    # add vars to spec
+    all_vars = vars + vars_opt
+    for var in all_vars:
         nd = np.ndim(data[var])
         if nd > len(dims):
             raise DimensionError("specified {} dimensions but data['{}'] is {}-dimensional".format(len(dims), var, nd))
-        out[var] = dict(dims=dims[0:nd], data=data[var])
+        spec[var] = dict(dims=dims[0:nd], data=data[var])
+
+    return xr.Dataset.from_dict(spec)
