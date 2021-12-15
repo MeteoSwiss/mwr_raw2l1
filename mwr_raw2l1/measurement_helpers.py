99e619b7866e15f60c7a9f501a21a22b8c10d458
rruefenacht <rruefenacht@users.noreply.github.com>
2021-12-15 08:26:48 +0100

    continue work on scan to timeseries conversion

--- mwr_raw2l1/measurement_helpers.py
+++ mwr_raw2l1/measurement_helpers.py
@@ -76,7 +76,7 @@ def rpg_to_datasets(data, dims, vars, vars_opt):
         for dat in data_series:  # make a xarray dataset from the data dict in each class instance
             data_act.append(make_dataset(dat.data, dims[src], vars[src], vars_opt[src]))
         out[src] = xr.concat(data_act, dim='time')  # merge all datasets of the same type
-        out[src] = drop_duplicates(out[src], dim='time')
+        out[src] = drop_duplicates(out[src], dim='time')  # remove duplicate measurements
     return out
 
 
@@ -90,12 +90,13 @@ def scan_endtime_to_time(endtime, n_angles, inttime=40, idletime=1.4):
             Corresponds ot the single timestamp saved with each angle scan. Assumed as the end time of the scan
         n_angles : number of angles per scan.
         inttime : integration time at each angle in seconds. The default is 40.
-        idletime : ime duration for moving the pointing to the respective scan position. The default is 1.4.
+        idletime : time duration for moving the pointing to the respective scan position in s. The default is 1.4.
 
     Returns:
         time : np.array of datetime.datetime objects. list of timestamps (end of integration) for each observed angle
     """
 
+    # TODO: add test to check that brt and blb times do not overlap (or think about it)
     if isinstance(endtime, dt.datetime):
         endtime = np.array([endtime])
         def timedelta_method(seconds): return dt.timedelta(seconds=seconds)
@@ -112,26 +113,38 @@ def scan_endtime_to_time(endtime, n_angles, inttime=40, idletime=1.4):
     return time
 
 
-def scan_to_timeseries_from_aux(blb, brt, hkd=None):
+def scan_to_timeseries_from_aux(blb, brt=None, hkd=None):
 
     # determine scan duration
     time_scan = blb['time'].values
     n_ele = len(blb['scan_ele'].values)
-    time_per_ele = scan_endtime_to_time(time_scan, n_ele)
-    # TODO: better infer optional paramters to scan_endtime_to_time from brt (or hkd)
-    # find last time in brt before scan: brt.sel(time=blb['time'][ind_act], method='pad')
-    # find first time in brt after scan: brt.sel(time=blb['time'][ind_act], method='backfill')
-    # ind_act = 0 (-1) might cause an error with pad (backfill)
-    # don't forget to remove elevation dimension
 
-    # TODO: use scan_to_timeseries here
+    endtime2time_params = dict(endtime=blb['time'].values, n_angles=len(blb['scan_ele']))
+    if hkd is not None and 'BLscan_active' in hkd:
+        time_scan_active = hkd['time'][hkd['BLscan_active'].values == 1]
+        # TODO: develop further for infering inttime/caltime/idletime. and replace below placeholders
+        endtime2time_params['inttime'] = 40
+        endtime2time_params['idletime'] = 1.4
+    elif brt is not None:
+        pass
+        # TODO: infer optional paramters to scan_endtime_to_time from brt (or hkd)
+        # find last time in brt before scan: brt.sel(time=blb['time'][ind_act], method='pad')
+        # find first time in brt after scan: brt.sel(time=blb['time'][ind_act], method='backfill')
+        # ind_act = 0 (-1) might cause an error with pad (backfill)
 
-    # TODO: return updated brt
+    time_per_ele = scan_endtime_to_time(**endtime2time_params)
+    # TODO: instead of scan_endtime_to_time(**endtime2time_params) call blb = scan_to_timeseries(blb, **endtime2time_params) here
 
+    # TODO: return updated blb
+    return blb
 
-def scan_to_timeseries(data):
-    """transform scans to time series of spectra and temperatures"""
-    # TODO find a place where this function should go (maybe reader_rpg_helpers and call after read in also hkd)
+def scan_to_timeseries(data, *args, **kwargs):
+    """transform scans to time series of spectra and temperatures
+
+    Args:
+        data: xarray.Dataset containing the scan observations (BLB)
+        *arrgs/**kwargs: optional parameters for scan_endtime_to_time
+    """
     n_freq = data['n_freq']
     n_ele = data['n_ele']
     n_scans = data['n_scans']
@@ -145,4 +158,29 @@ def scan_to_timeseries(data):
     data['ele'] = np.tile(data['scan_ele'], data['n_scans'])
 
     # time is encoded as end time of scan (same time for all elevations). need to transform to time series
-    # data['time'] = scan_endtime_to_time(data['time'], data['n_ele'])  # TODO: vectorise scan starttime_to_time (and write test)
+    data['time'] = scan_endtime_to_time(data['time'], data['n_ele'], *args, **kwargs)
+
+
+def scan_to_timeseries_from_dict(data, *args, **kwargs):
+    """transform scans to time series of spectra and temperatures
+
+    Args:
+        data: dictioinary containing the scan observations (BLB)
+        *arrgs/**kwargs: optional parameters for scan_endtime_to_time
+    """
+    n_freq = data['n_freq']
+    n_ele = data['n_ele']
+    n_scans = data['n_scans']
+
+    tb_tmp = data['Tb_scan'].swapaxes(0, 1)  # swap dim to (freq, time, ele)
+    data['Tb'] = tb_tmp.reshape(n_freq, n_scans * n_ele, order='C').transpose()
+
+    data['T'] = data['T_per_scan'].repeat(n_ele)  # repeat to have one T value for each new time
+
+    # transform single vector of elevations to time series of elevations
+    data['ele'] = np.tile(data['scan_ele'], data['n_scans'])
+
+    # time is encoded as end time of scan (same time for all elevations).
+    data['time'] = scan_endtime_to_time(data['time'], data['n_ele'])
+
+    return data
