e97fbd37ae08fccba031aa032faf7604bd872721
rruefenacht <rruefenacht@users.noreply.github.com>
2022-04-01 22:00:10 +0200

    extract method for generating uniqe time series

--- mwr_raw2l1/measurement/measurement_helpers.py
+++ mwr_raw2l1/measurement/measurement_helpers.py
@@ -14,25 +14,18 @@ def radiometrics_to_datasets(data_all, dims, vars, vars_opt):
              containing a series of instances of read-in classes.
         dims: list of keys that are a dimension (must correspond to the order of dimensions in data)
         vars: list of keys that are data variables (dimensions don't need to be specified again)
-        vars_opt: list of keys that are optional data variables (added as 1-dim series of NaN if missing in 'data')
+        vars_opt: list of keys that are optional data variables
     Returns:
         dictionary with one :class:`xarray.Dataset` for each key. It contains one item for each key in data
     """
 
-    out = {}
-
     if not isinstance(data_all, list):  # accept also single instances of read-in class not inside a list
         data_all = [data_all]
 
+    out = {}
     sources = data_all[0].data.keys()
-
     for src in sources:
-        data_act = []
-        for dat in data_all:
-            data_act.append(make_dataset(dat.data[src], dims[src], vars[src], vars_opt[src]))
-        out[src] = xr.concat(data_act, dim='time')  # merge all datasets of the same type
-        out[src] = drop_duplicates(out[src], dim='time')  # remove duplicate measurements
-
+        out[src] = to_single_dataset([dat.data[src] for dat in data_all], dims[src], vars[src], vars_opt[src])
     return out
 
 
@@ -139,6 +132,22 @@ def make_dataset(data, dims, vars, vars_opt=None, multidim_vars=None, time_vecto
         spec[var] = dict(dims=dims[0:nd], data=data[var])
 
     return xr.Dataset.from_dict(spec)
+
+
+def to_single_dataset(data_dicts, *args, **kwargs):
+    """return a single :class:`xarray.Dataset` with unique time vector from a list of data dictionaries
+
+    Args:
+        data_dicts: list of data dictionaries to be concatenated to a time series
+        *args: dimension and variable specifications passed on to :func:`make_dataset`
+        **kwargs: dimension and variable specifications passed on to :func:`make_dataset`
+    """
+    datasets = []
+    for dat in data_dicts:
+        datasets.append(make_dataset(dat, *args, **kwargs))
+    out = xr.concat(datasets, dim='time')  # merge all datasets of the same type
+    out = drop_duplicates(out, dim='time')  # remove duplicate measurements
+    return out
 
 
 def merge_aux_data(mwr_data, all_data, srcs_to_ignore=None):
