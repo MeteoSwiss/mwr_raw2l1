ca3a921f0c242269058d5f42f3d29373e0476404
rruefenacht <rruefenacht@users.noreply.github.com>
2022-05-27 18:19:42 +0200

    first steps to add entry point

---
+++ mwr_raw2l1/__main__.py
@@ -0,0 +1,4 @@
+from mwr_raw2l1 import main
+
+if __name__ == '__main__':
+    main.main('mwr_raw2l1/config/config_0-20000-0-99999_A.yaml', 'mwr_raw2l1/config/L1_format.yaml', 'mwr_raw2l1/config/qc_config.yaml')
