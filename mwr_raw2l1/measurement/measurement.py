MeteoSwiss/mwr_raw2l1

commit 98dd8f830d365819724d3b8c503fb532e137bd38

set IR wavelength in config instead of Radiometrics reader

mwr_raw2l1/measurement/measurement.py
@@ -46,9 +46,9 @@ class Measurement(MeasurementConstructors):
         self.set_vars(varname_data_conf, delta_data_conf, **kwargs)
 
     def set_wavelength(self, delta=1, **kwargs):
-        """(re)set wavelength of infrared radiometer in self.data accounting for self.conf_inst
+        """(re)set wavelength of infrared radiometer in self.data accounting for self.conf_inst. Also add as dimension.
 
-        If both are available the method checks consistency between wavelength in datafile and configuration.
+        If conf and data are available the method checks consistency between wavelength in datafile and configuration.
         For self.conf_inst to be usable it must contain key ir_wavelength.
         If self.conf_inst is None, variables are taken from datafile without any checks and an error is raised if any
         variable is missing in data.

mwr_raw2l1/config/config_0-20000-0-06610_A.yaml
@@ -29,7 +29,7 @@ beamwidth: [.NaN, .NaN, .NaN, .NaN, .NaN, .NaN, .NaN,
             .NaN, .NaN, .NaN, .NaN, .NaN, .NaN, .NaN]
 ir_bandwidth: .NaN
 ir_beamwidth: .NaN
-# ir_wavelength: # not mandatory to specify if available in data
+# ir_wavelength: # in nm. not mandatory to specify if available in data
 
 # the following parameters are potentially varying with time:
 freq_shift:  [0.000, 0.000, 0.000, 0.000, 0.000, 0.000, 0.000,  # K-band; in GHz

mwr_raw2l1/config/config_0-20000-0-10393_A.yaml
@@ -13,6 +13,7 @@ station_latitude: 52.21
 station_longitude: 14.12
 station_altitude: 98.
 # TODO: add instrument params as for Payerne
+ir_wavelength: 10.5  # in nm. not mandatory to specify if available in data
 
 nc_attributes:
   # title: do not set 'title' attribute unless you have a very good reason to deviate from the default

mwr_raw2l1/measurement/measurement_constructors.py
@@ -56,7 +56,7 @@ class MeasurementConstructors(object):
         """
         # dimensions and variable names for usage with make_dataset
         dims = {'mwr': ['time', 'frequency'],
-                'aux': ['time', 'ir_wavelength']}  # TODO: wavelength was added in reader, if from config better here
+                'aux': ['time']}
         vars = {'mwr': ['Tb', 'ele', 'azi', 'quality'],
                 'aux': ['IRT', 'p', 'T', 'RH', 'rainflag', 'quality']}
         vars_opt = {'mwr': [],

mwr_raw2l1/readers/reader_radiometrics.py
@@ -56,7 +56,6 @@ class Reader(object):
         data = get_data(self.data_raw[rec_type_nb], self.header['col_headers'][rec_type_nb], no_mwr=True)
         check_vars(data, mandatory_vars)
         self.data['aux'] = data
-        self.add_ir_wavelength()
 
     def _read_header(self, csv_lines):
         """read the header of the csv data (all 10-divisible record type numbers and 99)"""
@@ -108,11 +107,6 @@ class Reader(object):
             self.data_raw[rec_type] = x
         for rec_type in empty_rec:
             del self.data_raw[rec_type]
-
-    def add_ir_wavelength(self):
-        """add the wavelength of IR sensor from config"""
-        self.data['aux']['ir_wavelength'] = np.array([10.5])
-        # TODO: externalise to config and only set in Measurement
 
 
 def read_multiple_files(files):

commit 9478712f764b4d590707efc26df6723be3bc6590

bugfix to allow missing IRT (broken since 98dd8f8)

mwr_raw2l1/measurement/measurement.py
@@ -66,7 +66,7 @@ class Measurement(MeasurementConstructors):
 
         varname_data_conf = {varname_wavelength: 'ir_wavelength'}
         delta_data_conf = {varname_wavelength: delta}
-        self.set_vars(varname_data_conf, delta_data_conf, dim=varname_wavelength, **kwargs)
+        self.set_vars(varname_data_conf, delta_data_conf, dim=varname_wavelength, accept_unset=True, **kwargs)
 
     def set_receivers(self):
         """set receiver dimension and receiver-specific variables"""