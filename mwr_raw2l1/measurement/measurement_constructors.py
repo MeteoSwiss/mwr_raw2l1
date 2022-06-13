MeteoSwiss/mwr_raw2l1

commit e8fbcd25a71515ebfe561e75d0018c1fc0312db4

for RPG: if possible use T from hkd and set azi for scan

mwr_raw2l1/measurement/measurement_constructors.py
@@ -4,6 +4,7 @@ from mwr_raw2l1.errors import MissingDataSource
 from mwr_raw2l1.log import logger
 from mwr_raw2l1.measurement.measurement_construct_helpers import (attex_to_datasets, merge_aux_data, merge_brt_blb,
                                                                   radiometrics_to_datasets, rpg_to_datasets)
+from mwr_raw2l1.measurement.measurement_helpers import is_full_var_in_data, is_var_in_data
 from mwr_raw2l1.measurement.scan_transform import scan_to_timeseries_from_scanonly, scanflag_from_ele
 
 DTYPE_SCANFLAG = 'u1'  # data type used for scanflags set by Measurement class
@@ -94,7 +95,7 @@ class MeasurementConstructors(object):
                 'met': ['time'],
                 'hkd': ['time', 'channels']}
         vars = {'brt': ['Tb', 'rainflag', 'ele', 'azi'],
-                'blb': ['Tb', 'T', 'rainflag', 'scan_quadrant'],  # TODO: ask Bernhard: find way to infer 'azi' from blb
+                'blb': ['Tb', 'T', 'rainflag', 'scan_quadrant'],
                 'irt': ['IRT', 'rainflag', 'ele', 'azi'],  # ele/azi will become ele_irt/azi_irt as also present in MWR
                 'met': ['p', 'T', 'RH'],
                 'hkd': ['alarm']}
@@ -108,6 +109,7 @@ class MeasurementConstructors(object):
                             'Tstab_ok_kband', 'Tstab_ok_vband', 'Tstab_ok_amb']}
 
         scanflag_values = {'brt': 0, 'blb': 1}  # for generating a scan flag indicating whether scanning or zenith obs
+        max_azi_offset = 1  # maximum offset between all values in azimuth vector to be considered identical (degrees)
 
         logger.info('Creating instance of Measurement class from RPG data')
 
@@ -134,6 +136,18 @@ class MeasurementConstructors(object):
         # take mean of ambient temperature load (one load with two temperature sensors (code works with up to 9))
         tamb_vars = [var for var in data.data_vars if var[:-1] == 'T_amb_']
         t_amb = data[tamb_vars].to_array(dim='tmpdim').mean(dim='tmpdim', skipna=True)
+
+        # fix for variables potentially only available in blb or brt
+        if not is_full_var_in_data(data, 'T') and is_var_in_data(data, 'T_met'):
+            # above double check normally much faster than is_full_var_in_data(data, 'T_met')
+            data['T'] = data['T_met']
+        azi_med = data['azi'].median(skipna=True)  # outside if-clause for re-using below
+        if any(data['azi'].isnull()) and (data['azi'].max(skipna=True) - data['azi'].min(skipna=True)) < max_azi_offset:
+            data['azi'] = data['azi'].fillna(azi_med)
+
+        # apply scan_quadrant for blb azimuth (azi for scan_quadrant=1 already correct)
+        data['azi'][data['scan_quadrant'] == 2] = np.mod(azi_med + 180, 360)
+        data['azi'][data['scan_quadrant'] == 0] = np.nan
 
         # set receiver-dependent variables (must end with _receiver_n where n=1..9)
         data['T_amb_receiver_1'] = t_amb

mwr_raw2l1/main.py
@@ -137,8 +137,8 @@ def get_meas_constructor(name):
 
 
 if __name__ == '__main__':
-    run('config/config_0-20000-0-99999_A.yaml', 'config/L1_format.yaml', 'config/qc_config.yaml')  # Attex
-    run('config/config_0-20000-0-10393_A.yaml', 'config/L1_format.yaml', 'config/qc_config.yaml')  # Radiometrics
+    #run('config/config_0-20000-0-99999_A.yaml', 'config/L1_format.yaml', 'config/qc_config.yaml')  # Attex
+    #run('config/config_0-20000-0-10393_A.yaml', 'config/L1_format.yaml', 'config/qc_config.yaml')  # Radiometrics
     fs, ff = run('config/config_0-20000-0-06610_A.yaml', 'config/L1_format.yaml', 'config/qc_config.yaml')  # RPG HATPRO
 
     # For generating reference output file for test_rpg:

mwr_raw2l1/measurement/measurement_helpers.py
@@ -10,6 +10,11 @@ def is_var_in_data(data, var):
     return var in data and not all(np.isnan(data[var]))
 
 
+def is_full_var_in_data(data, var):
+    """Return False if 'var' is not in data or if any value of data['var'] is NaN, otherwise True"""
+    return var in data and not any(np.isnan(data[var]))
+
+
 def channels2receiver(freq, band_limits=None, base=1):
     """attribute receiver numbers (1, 2, ...) to frequency channels according to frequency bands
 

commit a2614ab7f760ffd1ecd85a9f9c2a7ded3c11043b

bugfix to allow missing BLB (broken since e8fbcd2)

mwr_raw2l1/measurement/measurement_constructors.py
@@ -146,8 +146,9 @@ class MeasurementConstructors(object):
             data['azi'] = data['azi'].fillna(azi_med)
 
         # apply scan_quadrant for blb azimuth (azi for scan_quadrant=1 already correct)
-        data['azi'][data['scan_quadrant'] == 2] = np.mod(azi_med + 180, 360)
-        data['azi'][data['scan_quadrant'] == 0] = np.nan
+        if 'scan_quadrant' in data:  # mandatory for BLB, but will not be present for BRT, where azi can be left as is
+            data['azi'][data['scan_quadrant'] == 2] = np.mod(azi_med + 180, 360)
+            data['azi'][data['scan_quadrant'] == 0] = np.nan
 
         # set receiver-dependent variables (must end with _receiver_n where n=1..9)
         data['T_amb_receiver_1'] = t_amb